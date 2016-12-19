e7dc5d0c34b4b5c4fcef0ca02f074b9862622e34
YouRenHsu <s11113153@stu.edu.tw>
2016-12-19 15:43:03 +0800

    <DoubleLinkList>   initialize resporitory

---
+++ DoubleLinkList/src/main.cpp
@@ -0,0 +1,19 @@
+#ifdef __cplusplus
+extern "C" {  
+#endif
+// include c lib
+#include <stdio.h>
+
+#ifdef __cplusplus
+} 
+#endif
+// include c++ lib
+#include <iostream>
+#include <cstdlib>
+
+
+int main(int argc, char const *argv[]) {
+  
+  return EXIT_SUCCESS;
+}
+
